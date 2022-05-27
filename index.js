a090851f5b26e834121e82677c1cc30a191e01c4
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-05-27 11:41:11 +0300

    feat: header menu burger is ready

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import './index.sass'
+import './style/index.sass'
+
+const burger = document.querySelector(['.burger'])
+const menu = document.getElementById("menu-list")
+
+burger.addEventListener('click',()=>{
+    menu.classList.contains('active')?menu.classList.remove('active'):menu.classList.add('active')
+})
